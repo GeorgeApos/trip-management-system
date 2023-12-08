b90543110fb3f9d646dc1890019fe41387004034
GeoApos <iis20001@uom.edu.gr>
2023-12-08 19:39:57 +0200

    Spring Initialization

---
+++ src/test/java/gr/uom/tripmanagementsystem/TripManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.uom.tripmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
